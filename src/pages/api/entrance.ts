baf00af8994030d0a1b2a58c9872b3b4d683d6eb
Kazuumi Nishimura <mail@kazuumi.page>
2022-10-13 20:43:48 +0900

    バックエンド修正

--- src/pages/api/entrance.ts
+++ src/pages/api/entrance.ts
@@ -4,39 +4,81 @@ import client from "lib/prismadb"
 
 // @ts-ignore
 const entranceApi = async (req, res) => {
-  // @ts-ignore
-  const token = await getToken({ req })
-  if (token) {
-    // Signed in
-    const user = await getEntrance(token)
-    res.status(200).json(user)
-  } else {
-    // Not Signed in
-    res.status(401)
+  switch (req.method) {
+    case 'GET':
+      // @ts-ignore
+      const token = await getToken({ req })
+      if (token) {
+        const user = await getEntrance(token)
+        res.status(200).json(user)
+      } else {
+        // サインインしていない
+        res.status(401)
+      }
+      return
+    case 'PATCH':
+      res.status(200).json({ message: 'PATCH' })
+      break
+
+    default:
+      res.status(405).end()
+      break
   }
 }
 
-const getEntrance = (token: any) => {
+const getEntrance = async (token: any) => {
   const { sub } = token;
-  const userType = checkUserType(token)
-  if (userType === 'nokodaisei') {
-    return {
-      userType: userType,
-      reserved: true,
-      eleventh: 1,
-      twelfth: 0,
-      thirteenth: 2,
-    }
-  } else {
-    return {
-      userType: userType,
-      reserved: true,
-      eleventh: 1,
-      twelfth: 1,
-      thirteenth: 0,
-      accompaniers: 2,
-    }
+  let entrance = await client.entrance.findUnique({
+    where: {
+      attendeeId: sub
+    },
+  })
+  // エントランスが存在しない場合
+  if (!entrance) {
+    entrance = await client.entrance.create({
+      data: {
+        attendeeId: sub,
+      }
+    })
   }
+  return {
+    userType: checkUserType(token),
+    reserved: !!entrance.updatedAt,
+    eleventh: entrance.eleventh,
+    twelfth: entrance.twelfth,
+    thirteenth: entrance.thirteenth,
+    accompaniers: entrance.accompaniers,
+  }
+}
+
+const patchEntrance = async (token: any, data: any) => {
+  const { sub } = token;
+  let { eleventh, twelfth, thirteenth, accompaniers } = data;
+
+  // TODO: 実際はDBをロックしてカウントして、などの処理が必要
+  // TODO: 味噌乳酸菌を注文している人は日曜日を取り消せない
+  if (checkUserType(token) === 'nokodaisei') {
+    eleventh = [0, 1, 2].includes(eleventh) ? eleventh : 0;
+    twelfth = [0, 1, 2].includes(twelfth) ? twelfth : 0;
+    thirteenth = [0, 1, 2].includes(thirteenth) ? thirteenth : 0;
+    accompaniers = 0;
+  } else {
+    eleventh = [0, 1].includes(eleventh) ? eleventh : 0;
+    twelfth = [0, 1].includes(twelfth) ? twelfth : 0;
+    thirteenth = [0, 1].includes(thirteenth) ? thirteenth : 0;
+    accompaniers = [0,1,2,3,4].includes(accompaniers) ? accompaniers : 0;
+  }
+  await client.entrance.update({
+    where: {
+      id: sub
+    },
+    data: {
+      eleventh,
+      twelfth,
+      thirteenth,
+      accompaniers,
+    }
+  })
 }
 
 export default entranceApi
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -4,31 +4,101 @@ import client from "lib/prismadb"
 
 // @ts-ignore
 const indexApi = async (req, res) => {
-  // @ts-ignore
-  const token = await getToken({ req })
-  if (token) {
-    // Signed in
-    const user = await getId(token)
-    res.status(200).json(user)
-  } else {
-    // Not Signed in
-    res.status(401)
+  switch (req.method) {
+    case 'GET':
+      // @ts-ignore
+      const token = await getToken({ req })
+      if (token) {
+        const user = await getId(token)
+        res.status(200).json(user)
+      } else {
+        // サインインしていない
+        res.status(401)
+      }
+      return
+    case 'POST':
+      res.status(200).json({ message: 'POST' })
+      break
+
+    case 'PATCH':
+      res.status(200).json({ message: 'PATCH' })
+      break
+
+    case 'DELETE':
+      res.status(200).json({ message: 'PATCH' })
+      break
+
+    default:
+      res.status(405).end()
+      break
   }
 }
 
 const getId = async (token: any) => {
   const { sub } = token;
-  const userType = checkUserType(token)
-  const numberId = '12345678' // TODO: get id from sub
-  const longerId = 'abcdefgh12345678' // TODO: get longerId from sub
-  const entranceReserved = true
-  const shoppingReserved = false
+  
+  // ユーザーを取得
+  let user = await client.attendee.findUnique({
+    where: {
+      id: sub
+    },
+    include: {
+      entrance: {
+        select: {
+          eleventh: true,
+          twelfth: true,
+          thirteenth: true,
+        }
+      },
+      shopping: {
+        select: {
+          original: true,
+          sour: true,
+          miso: true,
+          lactic: true,
+        }
+      },
+    }
+  })
+
+  //　ユーザーが存在しない場合
+  if (!user) {
+    // ユーザーを作成するために、ユニークな数字8桁の文字列を生成
+    let tempNumberId = "00000000"
+    while (true) {
+      // 該当のnumberIdを持たないかチェック
+      tempNumberId = Math.floor(Math.random() * 100000000).toString()
+      const checkUser = await client.attendee.findUnique({
+        where: {
+          numberId: tempNumberId
+        }
+      })
+      if (!checkUser) {
+        // 持たないことが確認できたのでbreak
+        break
+      }
+    }
+    // @ts-ignore
+    user = await client.attendee.create({
+      data: {
+        id: sub,
+        numberId: tempNumberId,
+        entrance: {
+          create: {}
+        },
+        shopping: {
+          create: {},
+        }
+      }
+    })
+  }
+
   return {
-    numberId,
-    longerId,
-    userType,
-    entranceReserved,
-    shoppingReserved,
+    numberId: user?.numberId,
+    longerId: user?.longerId,
+    userType: checkUserType(token),
+    entranceReserved: !!(user?.entrance?.eleventh || user?.entrance?.twelfth || user?.entrance?.thirteenth),
+    shoppingReserved: !!(user?.shopping?.original || user?.shopping?.sour || user?.shopping?.miso || user?.shopping?.lactic)
   }
 }
 
