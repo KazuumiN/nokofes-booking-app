c62a7e584ec804708d8f16ddd22166614371ae89
Kazuumi Nishimura <mail@kazuumi.page>
2022-10-18 13:01:34 +0900

    認証を予めやるハックは更新日時で対応

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,13 +8,6 @@ import prisma from "lib/prismadb"
 
 const { NEXTAUTH_SECRET } = process.env;
 
-const checkIfStarted = () => {
-  if (new Date("2022-10-20T12:58:00+0900") > new Date()) {
-    return false
-  }
-  return true
-}
-
 const getLineProfile = async (accessToken: string) => {
   const url = `https://api.line.me/v2/profile`
   const profile = await fetch(url, {
@@ -36,9 +29,6 @@ const getLineProfile = async (accessToken: string) => {
 }
 
 const sendVerificationViaGAS = async (email: string, url: string) => {
-  if (!checkIfStarted()) {
-    throw new Error('まだ開始していません')
-  }
   const endpoint = process.env.GAS_ENDPOINT
   const params = {
     email,
@@ -55,7 +45,6 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      // TODO: コードをダイエットできないか検討
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
       sendVerificationRequest({
@@ -72,9 +61,6 @@ export const authOptions = {
         accessToken: { label: "Access Token", type: "text", placeholder: "Enter your access token" },
       },
       async authorize(credentials) {
-        if (!checkIfStarted()) {
-          throw new Error('まだ開始していません')
-        }
         if (!credentials) {
           throw new Error('No credentials provided')
         }
