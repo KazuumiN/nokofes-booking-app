1b6a1a9bd42ba861a9e7c8e81aeda2ad4eb67a35
Kazuumi Nishimura <mail@kazuumi.page>
2022-10-18 16:10:06 +0900

    プログレスバー追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,13 @@ import Head from "next/head";
 import Timer from "components/Timer";
 import { useRouter } from "next/router";
 import { ToastContainer } from 'react-toastify'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
