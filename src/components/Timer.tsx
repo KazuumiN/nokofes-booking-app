7141681c2b20a3486468743d965a32f59e1ab41a
Kazuumi Nishimura <mail@kazuumi.page>
2022-10-19 16:42:42 +0900

    Timerから「日」表示を隠す

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,7 +8,6 @@ const Timer = ({expiryTimestamp}:any) => {
     seconds,
     minutes,
     hours,
-    days,
   } = useTimer({ expiryTimestamp, onExpire: () => location.reload() });
 
 
@@ -16,10 +15,6 @@ const Timer = ({expiryTimestamp}:any) => {
     <div className="flex flex-col items-center justify-center m-3">
       <div className="text-sm md:text-2xl font-extralight text-center mr-8">予約開始まで</div>
       <div className="text-2xl md:text-7xl text-center flex w-full items-center justify-center">
-        <div className="w-24 mx-1 p-2 bg-white text-green-500 rounded-lg">
-            <div className="font-mono leading-none" x-text="days">{days}</div>
-            <div className="font-mono text-md md:text-3xl ">日</div>
-        </div>
         <div className="w-24 mx-1 p-2 bg-white text-green-500 rounded-lg">
             <div className="font-mono leading-none" x-text="hours">{hours}</div>
             <div className="font-mono text-md md:text-3xl ">時間</div>
