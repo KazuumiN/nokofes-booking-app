3a45957ce8fa42ac9ca10c944fce31132489e27e
Kazuumi Nishimura <mail@kazuumi.page>
2022-10-20 17:04:33 +0900

    時刻判定をコメントアウト

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -17,23 +17,29 @@ const Layout: NextPage<{ children: ReactNode }> = ({
   const router = useRouter()
   useEffect(() => {
     console.log('開発に興味があれば、何かご一緒できると嬉しいので西村(Twitter: @Kazuumi_N)にDMください！')
-
   }, [])
-  const expiryTimestamp = new Date("2022-10-20T12:00:00+0900")
-  if (expiryTimestamp > new Date()) {
-    // 予約開始前はタイマーを表示する早期リターン
-    return <><Head><title>予約開始までお待ちください！</title><link rel="icon" href="/favicon.ico" /></Head><Timer expiryTimestamp={expiryTimestamp} /></>
-  }
+
+  // 時刻判定はもう（10/20 16時前）には不要なので、コメントアウトしておく。
+  // const expiryTimestamp = new Date("2022-10-20T12:00:00+0900")
+  // if (expiryTimestamp > new Date()) {
+  //   // 予約開始前はタイマーを表示する早期リターン
+  //   return <><Head><title>予約開始までお待ちください！</title><link rel="icon" href="/favicon.ico" /></Head><Timer expiryTimestamp={expiryTimestamp} /></>
+  // }
+
+  // 読み込み中の際に不要なものを出さない。
   if (status === "loading") {
     return <div className=" text-center">読み込み中...</div>
   }
+
   if (router.pathname === '/verify-request') {
+    // 認証メールを送りました画面
     return (
       <main className="flex flex-col items-center max-w-sm mx-auto my-4 text-gray-900 sm:text-gray-700">
         {children}
       </main>
     )
   } else if (router.pathname === '/auth-error') {
+    // 認証エラー画面
     return (
       <main className="flex flex-col items-center max-w-sm mx-auto my-4 text-gray-900 sm:text-gray-700">
         {children}
