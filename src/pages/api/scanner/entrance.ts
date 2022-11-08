802509ee588ee78e644528362486778af348361b
Kazuumi Nishimura <mail@kazuumi.page>
2022-11-08 10:34:40 +0900

    当日扱いの追加

--- src/pages/api/scanner/entrance.ts
+++ src/pages/api/scanner/entrance.ts
@@ -7,46 +7,58 @@ const scanEntranceApi = async (req: NextApiRequest, res: NextApiResponse) => {
     case 'GET':
       res.setHeader('Access-Control-Allow-Origin', '*');
       const id = query.id as string;
-      const type = query.type as string;
       const place = query.place as string;
-      const user = type === 'number'
-        ? await client.attendee.findFirst({
-            where: { numberId: id },
-            select: {
-              id: true,
-              eleventh: true,
-              twelfth: true,
-              thirteenth: true,
-              accompaniers: true,
-            },
-          })
-        : await client.attendee.findFirst({
-            where: { longerId: id },
-            select: {
-              id: true,
-              eleventh: true,
-              twelfth: true,
-              thirteenth: true,
-              accompaniers: true,
-            },
-          });
-      if (!user) {
-        res.status(404).json({ message: 'not found' });
-        return;
+      if (id.startsWith('nokofesentrance')) {
+        res.status(200).json({
+          onTheDayApplication: true,
+        })
+        await client.actual.create({
+          data: {
+            attendeeId: id,
+            place: place,
+          },
+        })
+      } else {
+        const type = query.type as string;
+        const user = type === 'number'
+          ? await client.attendee.findFirst({
+              where: { numberId: id },
+              select: {
+                id: true,
+                eleventh: true,
+                twelfth: true,
+                thirteenth: true,
+                accompaniers: true,
+              },
+            })
+          : await client.attendee.findFirst({
+              where: { longerId: id },
+              select: {
+                id: true,
+                eleventh: true,
+                twelfth: true,
+                thirteenth: true,
+                accompaniers: true,
+              },
+            });
+        if (!user) {
+          res.status(404).json({ message: 'not found' });
+          return;
+        }
+        res.status(200).json({
+          eleventh: user.eleventh,
+          twelfth: user.twelfth,
+          thirteenth: user.thirteenth,
+          accompaniers: user.accompaniers,
+        })
+        await client.actual.create({
+          data: {
+            attendeeId: user.id,
+            place: place,
+          },
+        })
+        return
       }
-      res.status(200).json({
-        eleventh: user.eleventh,
-        twelfth: user.twelfth,
-        thirteenth: user.thirteenth,
-        accompaniers: user.accompaniers,
-      })
-      await client.actual.create({
-        data: {
-          attendeeId: user.id,
-          place: place,
-        },
-      })
-      return
     default:
       res.status(405).end()
       break
